9d0df87ee7dab506e03bf946ffc94e4027747ee2
brunodappbrasil <bruno@dappbr.com.br>
2020-06-18 20:21:38 -0300

    adicionando a estrutura do projeto

---
+++ controllers/Register.js
@@ -0,0 +1,26 @@
+const knex = require('../config')
+
+class Register {
+  async registar(req, res) {
+    let nome = req.body.nome
+    let senha = req.body.senha
+    const verifica = await knex('users')
+      .select('*')
+      .where({ nome })
+      .first()
+  
+    if (verifica == undefined) {
+      const result = await knex('users').insert({
+        nome,
+        senha
+      })
+      .first()
+      
+      res.send(`Parabéns, você se registrou com o username ${nome}, senha ${senha} e o id é o ${result}!`)
+    } else {
+      res.send('Já existe um usuário com esse nome, favor insira outro!')
+    }
+  }
+}
+
+module.exports = Register
